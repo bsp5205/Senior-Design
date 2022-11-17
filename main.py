c98a61cbd508b8aa566badff56c13f2eee53476e
Shotzwastaken <bsp13101@gmail.com>
2022-11-17 17:09:25 -0500

    Fixed CC and added weighted method and DIT

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
 import javalang
 import clang
-
 import utilities as util
 
 def calculate_McGabe_cyclomatic_complexity(tree):
     # https://www.theserverside.com/feature/How-to-calculate-McCabe-cyclomatic-complexity-in-Java
     cc = 1
-
+    cc_list = []
     # get relevant lists
-    method_list = util.custom_filter(tree, 'IfStatement')
-    iterative_list = util.custom_filter(tree, 'WhileStatement') + util.custom_filter(tree, 'ForStatement')
+    method_list = util.custom_filter(tree, 'MethodDeclaration')
+    # iterative_list = util.custom_filter(tree, 'WhileStatement') + util.custom_filter(tree, 'ForStatement')
 
     for method_num in range(len(method_list)):  # loop through if/else if/else statements
-        if hasattr(method_list[method_num], 'else_statement') and type(method_list[method_num].else_statement).__name__ == 'IfStatement':
-            cc += 1 # else if
-        elif hasattr(method_list[method_num], 'then_statement') and type(method_list[method_num].else_statement).__name__ == 'BlockStatement':
-            cc += 1 # else
-        else:
-            cc += 1 # if
+        body_list = method_list[method_num].body
+        for body_list_num in range(len(body_list)):
+            if isinstance(body_list[body_list_num], javalang.tree.IfStatement):
+                # print('if')
+                cc += 1  # if
+            if isinstance(body_list[body_list_num], javalang.tree.IfStatement) and hasattr(body_list[body_list_num], 'else_statement'):
+                # print('elseif')
+                cc += 1  # else if
+            if isinstance(body_list[body_list_num], javalang.tree.IfStatement) and hasattr(body_list[body_list_num], 'then_statement'):
+                # print('else')
+                cc += 1  # else
 
-    for iter_num in range(len(iterative_list)): # loop through list of for loops and while loops
-        cc += 1
+            if isinstance(body_list[body_list_num], javalang.tree.WhileStatement):
+                # print('while')
+                cc += 1  # WhileStatement
 
-    print('calculate_McGabe_cyclomatic_complexity:', cc)
-    return cc
+            if isinstance(body_list[body_list_num], javalang.tree.ForStatement):
+                # print('for')
+                cc += 1  # ForStatement
+
+        cc_list.append(cc)
+        cc = 1
+
+    return cc_list
 
 def calculate_SLOC(tree):
     sloc = 0
@@ -49,7 +60,7 @@ def calculate_SLOC(tree):
         sloc += 1
 
     # IfStatement +2, ElseStatement +=1
-    if_statement_list = util.custom_filter(tree, 'IfStatement')
+    if_statement_list = util.custom_filter(tree, 'MethodDeclaration')
     for if_statement_num in range(len(if_statement_list)):
         if hasattr(if_statement_list[if_statement_num], 'else_statement') and type(if_statement_list[if_statement_num].else_statement).__name__ == 'IfStatement':
             sloc += 2
@@ -68,31 +79,53 @@ def calculate_SLOC(tree):
     for while_num in range(len(while_statement_list)):
         sloc += 2
 
-    print('calculate_SLOC:', sloc)
     return sloc
 
 def calculate_comment_percentage(tree, number_of_comments):
     sloc = calculate_SLOC(tree)
-    print('calculate_comment_percentage:', (number_of_comments/sloc) * 100)
     if sloc > 0:
-        return (number_of_comments/sloc) * 100
+        return round((number_of_comments/sloc) * 100, 2)
     else:
         return 0
 
 def calculate_weighted_method_per_class(tree):
-    # total the CC of all of a class's methods
-    # get a list of all methods using filter
-    # for each method, calculate the CC
-    return 0
+    cc_list = calculate_McGabe_cyclomatic_complexity(tree)
+    return sum(cc_list)
+
+def recursive_check(tree, subtree, height):
+    # print(subtree)
+    # children_list = subtree.implements
+    # print(children_list)
+    implement_list = subtree.implements
+    for subtree_num in range(len(implement_list)):
+        # print(subtree.name, 'implements', subtree.implements[0].name)
+        class_list = util.custom_filter(tree, 'ClassDeclaration')
+        for class_num in range(len(class_list)):
+            # print('checking class', class_list[class_num].name)
+            if class_list[class_num].name == subtree.implements[0].name:
+                subtree = class_list[class_num]
+                return recursive_check(tree, subtree, height + 1)
+
+    return height
 
 def calculate_depth_of_inheritance(tree):
-    # assuming inheritance has something to do with the modifier attribute?
-    return 0
+    dit = 0
+    class_list = util.custom_filter(tree, 'ClassDeclaration')
+    for class_num in range(len(class_list)):
+        class_being_checked = class_list[class_num]
+        dit_new = recursive_check(tree, class_being_checked, 0)
+        if dit_new > dit:
+            dit = dit_new
+
+    return dit
 
 def calculate_coupling_between_objects(tree):
     # count number of times a different class's methods or attributes are used in the class in question
     # maybe something to do with member attribute?
-    return 0
+    cbo = 0
+
+
+    return cbo
 
 def main():
     # set the path
@@ -115,9 +148,11 @@ def main():
 
         # print the tree
         util.pretty_print(tree)
-        calculate_McGabe_cyclomatic_complexity(tree)
-        calculate_SLOC(tree)
-        calculate_comment_percentage(tree, comment_count)
-
+        print('calculate_McGabe_cyclomatic_complexity:', calculate_McGabe_cyclomatic_complexity(tree))
+        print('calculate_SLOC:', calculate_SLOC(tree))
+        print('calculate_comment_percentage:', calculate_comment_percentage(tree, comment_count))
+        print('calculate_weighted_method_per_class:', calculate_weighted_method_per_class(tree))
+        print('calculate_depth_of_inheritance:', calculate_depth_of_inheritance(tree))
+        print('calculate_coupling_between_objects:', calculate_coupling_between_objects(tree))
 if __name__ == '__main__':
     main()
