d8e12913cb99fcd55a31d8d1bb39c57a68710e59
Kenneth Mitchell Burt <kmb898@psu.edu>
2023-04-20 14:03:23 -0400

    Remove Files add Post

--- LocalServer.py
+++ LocalServer.py
@@ -3,12 +3,63 @@ import subprocess
 import flask
 import handle_request as hr
 from flask import Flask, request, jsonify, render_template_string, render_template
-#import report
+import report
 
 #from flask_cors import CORS
 app = Flask(__name__)
 #cors = CORS(app)
 
+def last(flask):
+    assigns, course, api = hr.launch_page(flask.request.form['custom_canvas_api_domain'],
+                                          flask.request.form['custom_canvas_course_id'])
+    print(assigns[0].attributes)
+
+    # this will be modularized
+    assign_id = assigns[0].attributes['id']
+    subs = hr.launch_assignment(course, assign_id)
+    # ^once launched foreach grab and remove the files to build the metrics dict
+    attachments = subs[0].attributes['attachments']
+
+    cur_ass = assigns[0]
+    assignment_info = {}
+
+    print(course.attributes)
+    # course section
+    course_name = course.attributes['name']
+    course_code = course.attributes['course_code']
+    assign_name = cur_ass.attributes['name']
+    canvas_date = cur_ass.attributes['due_at']
+
+    assignment_info["course_name"] = course_name
+    assignment_info["course_code"] = course_code
+    assignment_info["assign_name"] = assign_name
+    assignment_info["due_date"] = canvas_date
+
+    # get the students info
+    student_info = []
+    file_stu = {}
+    for submission in subs:
+        print(submission.attributes)
+        student_id = submission.attributes['user_id']
+        student = api.get_user(student_id)
+        try:
+            att = submission.attributes['attachments']
+        except:
+            print("No Attach")
+        else:
+            tcur_files = hr.get_submission_file(att)
+            file_stu[student.attributes['id']] = tcur_files
+            student_info.append((student.attributes['id'], student.attributes['name'], submission.attributes['id']))
+
+    # generating the metrics for all files. May take some time
+    report_metrics = hr.analyze_submissions(subs)
+
+    # first submission and files
+    cur_files = hr.get_submission_file(attachments)
+    print(cur_files)
+
+    # generate the air file
+    report.air_file(cur_files, student_info, assignment_info, report_metrics, file_stu)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -16,53 +67,46 @@ def index():
     print(flask.request.method)
     print(flask.request.form)
     if flask.request.method == "POST":
-        assigns, course, api = hr.launch_page(flask.request.form['custom_canvas_api_domain'], flask.request.form['custom_canvas_course_id'])
-        print(assigns[0].attributes)
-
-        # this will be modularized
-        assign_id = assigns[0].attributes['id']
-        subs = hr.launch_assignment(course, assign_id)
-        # ^once launched foreach grab and remove the files to build the metrics dict
-        attachments = subs[0].attributes['attachments']
-
-        cur_ass = assigns[0]
-        assignment_info = {}
-
-        print(course.attributes)
-        # course section
-        course_name = course.attributes['name']
-        course_code = course.attributes['course_code']
-        assign_name = cur_ass.attributes['name']
-        canvas_date = cur_ass.attributes['due_at']
-
-        assignment_info["course_name"] = course_name
-        assignment_info["course_code"] = course_code
-        assignment_info["assign_name"] = assign_name
-        assignment_info["due_date"] = canvas_date
-
-        # get the students info
-        student_info = []
-        for submission in subs:
-            student_id = submission.attributes['user_id']
-            student = api.get_user(student_id)
-            student_info.append((student.attributes['id'], student.attributes['name'], submission.attributes['id']))
-
-        # generating the metrics for all files. May take some time
-        report_metrics = hr.analyze_submissions(subs)
-
-        # first submission and files
-        cur_files = hr.get_submission_file(attachments)
-        print(cur_files)
-
-        # generate the air file
-        # report.air_file(cur_files, student_info, assignment_info, report_metrics)
-
+        last(flask)
         # Put a "your session has been opened in a separate tab" page here?
-        return render_template('home.html')
+        return render_template('airFile.html')
     # Possibly send to error page
     return render_template('airFile.html')
 
     # return render_template_string(HTML String from Airium)
+
+@app.route("/next_student_endpoint", methods=['POST'])
+def f():
+    print(flask.request.form)
+    stu_id = int(flask.request.form['student_list'])
+
+    report.go_here(stu_id, 0)
+    return render_template('airFile.html')
+
+@app.route("/prev_student_endpoint", methods=['POST'])
+def p():
+    print(flask.request.form)
+    stu_id = int(flask.request.form['student_list'])
+
+    report.go_here(stu_id, 0)
+    return render_template('airFile.html')
+
+@app.route("/change_student", methods=['POST'])
+def d():
+    print(flask.request.form)
+    stu_id = int(flask.request.form['student_list'])
+
+    report.go_here(stu_id, 0)
+    return render_template('airFile.html')
+
+@app.route("/change_file", methods=['POST'])
+def t():
+    print(flask.request.form)
+    stu_id = int(flask.request.form['student_id'])
+    file_id = int(flask.request.form['file_id'])
+
+    report.go_here(stu_id, file_id)
+    return render_template('airFile.html')
 
 
 def run_app():
