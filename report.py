f20f5fad1f9148b2459cb4168abe2f77ad076ed8
Shotzwastaken <bsp13101@gmail.com>
2023-04-23 14:25:43 -0400

    style bug fixes

--- report.py
+++ report.py
@@ -428,16 +428,13 @@ def go_here(guy, fill, thresh):
                             for j in range(len(qma_metric_score_list[i].metric_scores)):
                                 with a.tr():
                                     with a.td(klass='table-title ' + qma_metric_score_list[i].instance_name):
+                                        with a.svg(klass='bi bi-question-circle question', fill='currentColor',height='16',viewbox='0 0 16 16', width='16', xmlns='http://www.w3.org/2000/svg'):
+                                            a.path(d='M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z')
+                                            a.path(d='M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z')
                                         a(qma_metric_score_list[i].metric_names[j])
-                                        with a.svg(klass='bi bi-question-circle question', fill='currentColor',
-                                                   height='16',
-                                                   viewbox='0 0 16 16', width='16', xmlns='http://www.w3.org/2000/svg'):
-                                            a.path(
-                                                d='M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z')
-                                            a.path(
-                                                d='M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z')
-                                        a.p(klass='metric-info-popup',
-                                            _t=qma_metric_score_list[i].metric_description[j])
+                                        with a.div(klass='test'):
+                                            a.p(klass='metric-info-popup',
+                                                _t=qma_metric_score_list[i].metric_description[j])
                                     with a.td(klass='progress-bar-cont'):
                                         a.div(klass='color-circle s' + str(counter + 1), role='progressbar',
                                               style='--value:' + str(focus_file.qma_scores[i][j]),
@@ -454,16 +451,13 @@ def go_here(guy, fill, thresh):
                             for j in range(len(cma_metric_score_list[i].metric_scores)):
                                 with a.tr():
                                     with a.td(klass='table-title ' + cma_metric_score_list[i].instance_name):
+                                        with a.svg(klass='bi bi-question-circle question', fill='currentColor',height='16', viewbox='0 0 16 16', width='16',xmlns='http://www.w3.org/2000/svg'):
+                                            a.path(d='M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z')
+                                            a.path(d='M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z')
                                         a(cma_metric_score_list[i].metric_names[j])
-                                        with a.svg(klass='bi bi-question-circle question', fill='currentColor',
-                                                   height='16',
-                                                   viewbox='0 0 16 16', width='16', xmlns='http://www.w3.org/2000/svg'):
-                                            a.path(
-                                                d='M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z')
-                                            a.path(
-                                                d='M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z')
-                                        a.p(klass='metric-info-popup',
-                                            _t=cma_metric_score_list[i].metric_description[j])
+                                        with a.div(klass='test'):
+                                            a.p(klass='metric-info-popup',
+                                                _t=cma_metric_score_list[i].metric_description[j])
                                     with a.td(klass='progress-bar-cont'):
                                         a.div(klass='color-circle s' + str(counter + 1), role='progressbar',
                                               style='--value:' + str(focus_file.cma_scores[i][j]),
