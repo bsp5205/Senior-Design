ed434af74e9db0491ce4d265d18071a5bcdb428c
Alex Anderson <ama7019@psu.edu>
2023-05-02 18:42:48 -0400

    Unit tests update

--- unit_tests.py
+++ unit_tests.py
@@ -9,43 +9,43 @@ class TestMetrics(unittest.TestCase):
 
     def test_CC_cyclomatic(self):
         path = "DNA_Holder/cyclomatic.java"
-        self.assertEqual(m.calculate_McGabe_cyclomatic_complexity(create_tree(path)), [(3, 8), (4, 9), (5, 13)])
+        self.assertEqual(m.calculate_McGabe_cyclomatic_complexity(create_tree(path)), [(3, 8), (4, 10), (5, 11)])
 
     def test_CC_DNA(self):
         path = "DNA_Holder/DNA.java"
-        self.assertEqual(m.calculate_McGabe_cyclomatic_complexity(create_tree(path)), [(5, 41), (9, 26)])
+        self.assertEqual(m.calculate_McGabe_cyclomatic_complexity(create_tree(path)), [(5,53), (9, 51)])
 
     def test_CC_DNA1(self):
         path = "DNA_Holder/DNA1.java"
-        self.assertEqual(m.calculate_McGabe_cyclomatic_complexity(create_tree(path)), [(2, 20)])
+        self.assertEqual(m.calculate_McGabe_cyclomatic_complexity(create_tree(path)), [(2, 21)])
 
     def test_CC_DNA2(self):
         path = "DNA_Holder/DNA2.java"
-        self.assertEqual(m.calculate_McGabe_cyclomatic_complexity(create_tree(path)), [(2, 17)])
+        self.assertEqual(m.calculate_McGabe_cyclomatic_complexity(create_tree(path)), [(2, 18)])
 
     def test_CC_DNA3(self):
         path = "DNA_Holder/DNA3.java"
-        self.assertEqual(m.calculate_McGabe_cyclomatic_complexity(create_tree(path)), [(1, 4), (1, 4), (16, 41), (3, 7), (2, 8), (2, 8)])
+        self.assertEqual(m.calculate_McGabe_cyclomatic_complexity(create_tree(path)), [(1, 1), (1, 1), (16, 73), (3, 11), (2, 6), (2, 6)])
 
     def test_SLOC_cyclomatic(self):
         path = "DNA_Holder/cyclomatic.java"
-        self.assertEqual(m.calculate_SLOC(create_tree(path)), 32)
+        self.assertEqual(m.calculate_SLOC(create_tree(path)), 30)
 
     def test_SLOC_DNA(self):
         path = "DNA_Holder/DNA.java"
-        self.assertEqual(m.calculate_SLOC(create_tree(path)), 69)
+        self.assertEqual(m.calculate_SLOC(create_tree(path)), 109)
 
     def test_SLOC_DNA1(self):
         path = "DNA_Holder/DNA1.java"
-        self.assertEqual(m.calculate_SLOC(create_tree(path)), 22)
+        self.assertEqual(m.calculate_SLOC(create_tree(path)), 25)
 
     def test_SLOC_DNA2(self):
         path = "DNA_Holder/DNA2.java"
-        self.assertEqual(m.calculate_SLOC(create_tree(path)), 19)
+        self.assertEqual(m.calculate_SLOC(create_tree(path)), 25)
 
     def test_SLOC_DNA3(self):
         path = "DNA_Holder/DNA3.java"
-        self.assertEqual(m.calculate_SLOC(create_tree(path)), 76)
+        self.assertEqual(m.calculate_SLOC(create_tree(path)), 106)
 
     def test_CP_cyclomatic(self):
         path = "DNA_Holder/cyclomatic.java"
@@ -55,22 +55,22 @@ class TestMetrics(unittest.TestCase):
     def test_CP_DNA(self):
         path = "DNA_Holder/DNA.java"
         concat_line, comment_count = util.read_file(path, 0)
-        self.assertEqual(m.calculate_comment_percentage(create_tree(path), comment_count), 33.33)
+        self.assertEqual(m.calculate_comment_percentage(create_tree(path), comment_count), 21.1)
 
     def test_CP_DNA1(self):
         path = "DNA_Holder/DNA1.java"
         concat_line, comment_count = util.read_file(path, 0)
-        self.assertEqual(m.calculate_comment_percentage(create_tree(path), comment_count), 31.82)
+        self.assertEqual(m.calculate_comment_percentage(create_tree(path), comment_count), 28.0)
 
     def test_CP_DNA2(self):
         path = "DNA_Holder/DNA2.java"
         concat_line, comment_count = util.read_file(path, 0)
-        self.assertEqual(m.calculate_comment_percentage(create_tree(path), comment_count), 36.84)
+        self.assertEqual(m.calculate_comment_percentage(create_tree(path), comment_count), 28.0)
 
     def test_CP_DNA3(self):
         path = "DNA_Holder/DNA3.java"
         concat_line, comment_count = util.read_file(path, 0)
-        self.assertEqual(m.calculate_comment_percentage(create_tree(path), comment_count), 56.58)
+        self.assertEqual(m.calculate_comment_percentage(create_tree(path), comment_count), 40.57)
 
     def test_WMC_cyclomatic(self):
         path = "DNA_Holder/cyclomatic.java"
@@ -100,11 +100,11 @@ class TestMetrics(unittest.TestCase):
 
     def test_DIT_DNA1(self):
         path = "DNA_Holder/DNA1.java"
-        self.assertEqual(m.calculate_depth_of_inheritance(create_tree(path)), 1)
+        self.assertEqual(m.calculate_depth_of_inheritance(create_tree(path)), 0)
 
     def test_DIT_DNA2(self):
         path = "DNA_Holder/DNA2.java"
-        self.assertEqual(m.calculate_depth_of_inheritance(create_tree(path)), 1)
+        self.assertEqual(m.calculate_depth_of_inheritance(create_tree(path)), 0)
 
     def test_DIT_DNA3(self):
         path = "DNA_Holder/DNA3.java"
@@ -176,6 +176,21 @@ class TestMetrics(unittest.TestCase):
         path = "DNA_Holder/DNA2.java"
         self.assertEqual(mood.calculate_coupling_factor(create_tree(path)), 0.0)
 
+    def test_COF_medium(self):
+        path = "TestAssignmentFiles/medium.java"
+        self.assertEqual(mood.calculate_coupling_factor(create_tree(path)), 3.0)
+
+    def test_COF_complex(self):
+        path = "TestAssignmentFiles/complicated.java"
+        self.assertEqual(mood.calculate_coupling_factor(create_tree(path)), 0.0)
+
+    def test_POF_medium(self):
+        path = "TestAssignmentFiles/medium.java"
+        self.assertEqual(mood.calculate_polymorphism_factor(create_tree(path)), 0.16666666666666666)
+
+    def test_POF_complex(self):
+        path = "TestAssignmentFiles/complicated.java"
+        self.assertEqual(mood.calculate_polymorphism_factor(create_tree(path)), 0.8333333333333334)
 
 def create_tree(path):
     concat_line, comment_count = util.read_file(path, 0)
